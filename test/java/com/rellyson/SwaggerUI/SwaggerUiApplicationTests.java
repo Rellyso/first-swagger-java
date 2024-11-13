df79ee6044d48e9a3c098254f74920274b1240f8
Rellyson <rellysondouglas2015@gmail.com>
2024-11-13 23:56:37 +0000

    configure mvn spring boot

---
+++ test/java/com/rellyson/SwaggerUI/SwaggerUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rellyson.SwaggerUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerUiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
